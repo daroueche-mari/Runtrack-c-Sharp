5f5c4c5aef5066cfd76dd2cf38d569fea2196fe9
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-09-01 16:01:13 +0200

    jour 2 job 6

---
+++ Jour 2/Job 6/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace Job4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("-----Card-----");
+
+
+            ReturnCard carte1 = new ReturnCard(0, 0);
+            ReturnCard carte2 = new ReturnCard(carte1);
+
+            Console.WriteLine(carte1.SetCard(0));
+            Console.WriteLine("Arrivé au premier donjon");
+            Console.WriteLine(carte1.AddCard(1));
+            Console.WriteLine("Le nombre total de carte est de :" + " " + ReturnCard.TotalCardIssued());
+            ReturnCard.GetPostRules();
+            Console.WriteLine("Arrivé au deuxième donjon");
+            Console.WriteLine(carte2.AddCard(1));
+            Console.WriteLine("Le nombre total de carte est de :" + " " + ReturnCard.TotalCardIssued());
+            ReturnCard.GetPostRules();
+
+            Console.WriteLine("-----TrapLocation-----");
+
+
+            TrapLocation currentPos = new TrapLocation(5, 9);
+            TrapLocation cible = new TrapLocation(3, 10);
+            TrapLocation backupPos = currentPos;
+
+            double distance = backupPos.DistanceTo(cible);
+            Console.WriteLine(distance);
+
+            Console.WriteLine("-----Monstre-----");
+
+            List<Monster> mylist = new List<Monster>
+            {
+                new Monster("", 0, false),
+                new Monster("", 0, false),
+                new Monster("", 0, false),
+                new Monster("", 0, false)
+
+            };
+
+            mylist[0].setMonster("Monstre1", 100, 1);
+            mylist[1].setMonster("Monstre2", 200, 0);
+            mylist[2].setMonster("Monstre3", 100, 0);
+            mylist[3].setMonster("Monstre4", 300, 0);
+
+            Console.WriteLine(mylist[0].getnameMonster());
+            Console.WriteLine(mylist[0].gethealthMonster());
+            Console.WriteLine(mylist[0].getstateMonster());
+
+            int test = mylist[0].health + mylist[1].health + mylist[2].health + mylist[3].health;
+            Console.WriteLine("Le nombre total de vie est de :" + " " + test);
+            var newtest = mylist.MaxBy(s =>s.health);
+            Console.WriteLine(newtest?.health);
+            Console.WriteLine(newtest?.name);
+        }
+    }
+}
