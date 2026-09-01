cdce71d37590f50a09a3972ac8258205ffa2ea6b
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-09-01 08:59:36 +0200

    Jour 1 Job 1 et 2

---
+++ Jour 1/Job 1/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Job1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Msg.Communicate("Hello World"));
+        }
+    }
+}
---
+++ Jour 1/Job 1/message.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Job1
+{
+    public class Msg
+    {
+        public static string Communicate(string message)
+        {
+            return message;
+        }
+    }
+}
