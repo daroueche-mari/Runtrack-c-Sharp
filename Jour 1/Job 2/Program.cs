5851abc14431abd56d568373af1df284867724d1
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-09-03 15:58:11 +0200

    jour 1 job 2

---
+++ Jour 1/Job 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Job2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string nom;
+            int age;
+            int niveau;
+            string classe;
+            double pieceor;
+            bool nouveau;
+        }
+    }
+}
